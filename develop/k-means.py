810acc5a13b4dc47aeaf8f77924283961ce46dd2
wintershii@163.com <wintershii@163.com>
2020-12-22 21:03:17 +0800

    二分k-means

--- develop/k-means.py
+++ develop/k-means.py
@@ -63,13 +63,13 @@ class KMeans:
             newCenters = list()
 
             for key in Cluster.keys():
-                newCenter = np.round(np.mean(Cluster[key]["values"]), 3) 
+                newCenter = np.round(np.mean(Cluster[key]["values"]), 3)
                 Cluster[key]["center"] = newCenter
                 newCenters.append(newCenter)
             print("第{}次迭代后的簇类中心为:{}, old:{}".format(i, newCenters, oldCenters))
             if operator.eq(oldCenters, newCenters) or i > maxIters:
                 clusterChanged = False
-                
+
             else:
                 oldCenters = newCenters
                 i += 1
@@ -78,25 +78,75 @@ class KMeans:
                     Cluster[key]["values"] = []
         return Cluster
 
+    # 计算对应的SSE值
+    def SSE(self, data, mean):
+        newData = np.mat(data) - mean
+        return (newData * newData.T).tolist()[0][0]
 
-def createData():
-    content = {}
-    content['skuid'] = []
-    content['price'] = []
+    # 二分k-Means
+    def diKMeans(self, data, K):
+        clusterSSEResult = dict()
+        clusterSSEResult.setdefault(0, {})
+        clusterSSEResult[0]["values"] = data
+        clusterSSEResult[0]["sse"] = np.inf
+        clusterSSEResult[0]["center"] = np.mean(data)
+        # print(clusterSSEResult[0])
+        while len(clusterSSEResult) < K:
+            maxSSE = -np.inf
+            maxSSEKey = 0
+            # 找到最大SSE值对应数据, 进行kmeans聚类
+            for key in clusterSSEResult.keys():
+                if clusterSSEResult[key]["sse"] > maxSSE:
+                    maxSSE = clusterSSEResult[key]["sse"]
+                    maxSSEKey = key
+            clusterResult = self.kMeans(clusterSSEResult[maxSSEKey]["values"],
+                                        K=2,
+                                        maxIters=200)
+            # 删除clusterSSE中的minKey对应的值
+            del clusterSSEResult[maxSSEKey]
+            # 将经过kMeans聚类后的结果赋值给clusterSSEResult
+            clusterSSEResult.setdefault(maxSSEKey, {})
+            # print(clusterSSEResult[0])
+            clusterSSEResult[maxSSEKey]["center"] = clusterResult[0]["center"]
+            clusterSSEResult[maxSSEKey]["values"] = clusterResult[0]["values"]
+            clusterSSEResult[maxSSEKey]["sse"] = self.SSE(
+                clusterResult[0]["values"], clusterResult[0]["center"])
 
-    for i in range(100):
-        content['skuid'].append(1000 + i)
-        content['price'].append(random.randint(10, 10000))
+            maxKey = max(clusterSSEResult.keys()) + 1
+            clusterSSEResult.setdefault(maxKey, {})
+            clusterSSEResult[maxKey]["center"] = clusterResult[1]["center"]
+            clusterSSEResult[maxKey]["values"] = clusterResult[1]["values"]
+            clusterSSEResult[maxKey]["sse"] = self.SSE(
+                clusterSSEResult[1]["values"], clusterResult[1]["center"])
 
-    df = pd.DataFrame(content, columns=['skuid', 'price'])
-    df.to_csv('C:\\Users\\mirac\\Desktop\\price.csv', index=False, sep=",")
+        return clusterSSEResult
 
 
 if __name__ == "__main__":
-    # createData()
     km = KMeans()
     file = 'C:\\Users\\mirac\\Desktop\\price.csv'
     data = km.loadData(file)
     newData, upper_limit, lower_limit = km.filterAnomalyValue(data)
-    Cluster = km.kMeans(newData["price"].values, K=7, maxIters=200)
+    Cluster = km.diKMeans(newData["price"].values, K=7)
     print(Cluster)
+
+# def createData():
+#     content = {}
+#     content['skuid'] = []
+#     content['price'] = []
+
+#     for i in range(100):
+#         content['skuid'].append(1000 + i)
+#         content['price'].append(random.randint(10, 10000))
+
+#     df = pd.DataFrame(content, columns=['skuid', 'price'])
+#     df.to_csv('C:\\Users\\mirac\\Desktop\\price.csv', index=False, sep=",")
+
+# if __name__ == "__main__":
+#     # createData()
+#     km = KMeans()
+#     file = 'C:\\Users\\mirac\\Desktop\\price.csv'
+#     data = km.loadData(file)
+#     newData, upper_limit, lower_limit = km.filterAnomalyValue(data)
+#     Cluster = km.kMeans(newData["price"].values, K=7, maxIters=200)
+#     print(Cluster)
